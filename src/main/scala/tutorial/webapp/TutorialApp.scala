be4f9ed3ac9b98e8134bce0b0e8319965b2e8675
Suhku Huh <nineclue@gmail.com>
2018-10-26 10:24:40 +0900

    button click에 대응

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -2,9 +2,24 @@ package tutorial.webapp
 
 import org.scalajs.dom
 import dom.document
+import scala.scalajs.js.annotation.JSExportTopLevel
 
 object TutorialApp {
+  val msg = "안녕하세요, 여러분!"
+  val msgBtn = "버튼을 누르셨군요!"
   def main(args: Array[String]): Unit = {
-    println("안녕하세요, 여러분!")
+    appendPar(document.body, msg)
+  }
+
+  def appendPar(targetNode: dom.Node, text: String) = {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
+  }
+
+  @JSExportTopLevel("addClickedMessage")
+  def addClickedMessage() = {
+    appendPar(document.body, msgBtn)
   }
 }
